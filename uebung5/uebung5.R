26ae9dc1ff9ff34c420e3fc751cfb7305277b7c3
AndreasADL <ahrghhh@t-online.de>
2022-06-20 03:57:33 +0200

    final version

--- uebung5/uebung5.R
+++ uebung5/uebung5.R
@@ -3,7 +3,8 @@ rm(list = ls())
 
 #### Exercise 1 ####
 
-# a.)
+# a.) ----
+
 # The time series needs to have a starting point... this could be random,
 # but why not start with 0?
 start_point <- 0
@@ -28,7 +29,7 @@ typeof(ar_matrix) # type of matrix is double != float with double bits
 dim(ar_matrix) # 200x100 matrix
 ar_matrix[1, ] # all the same for same seed
 
-# b.)
+# b.) ----
 
 acf_est <- function(h, x_vector) {
     num_obs <- length(x_vector)
@@ -43,7 +44,7 @@ a <- acf_hat(1, ar_matrix) / acf_hat(0, ar_matrix)
 plot(a)
 mean(a)
 
-# c.)
+# c.) ----
 ar_200 <- sapply(rep(200, 100), rar_1)
 ar_500 <- sapply(rep(500, 100), rar_1)
 ar_1000 <- sapply(rep(1000, 100), rar_1)
@@ -70,7 +71,7 @@ mean((a_1000 - 0.5) ** 2)
 
 #### Exercise 2 ####
 
-# a.)
+# a.) ----
 rar_2 <- function(num_obs, a=c(-1, -0.25), start_point=0) {
     # set.seed(1)
     rar <- rnorm(num_obs, 0, 1)
@@ -83,14 +84,14 @@ rar_2 <- function(num_obs, a=c(-1, -0.25), start_point=0) {
 
 ar_2_matrix <- sapply(rep(500, 100), rar_2)
 
-# b.)
+# b.) ----
 acf2_hat <- function(h) acf_hat(h, ar_matrix = ar_2_matrix)
 
 g0 <- acf2_hat(0)
 g1 <- acf2_hat(1)
 g2 <- acf2_hat(2)
 
-# To needlessly complicate matters we are going to use arrays-----
+# To needlessly complicate matters we are going to use arrays ----
 
 # Gamma needs to be 100x2x2 tensor/array
 gamma <- array(c(g0, g1, g1, g0), dim = c(100, 2, 2))
@@ -129,7 +130,7 @@ plot(a[, 1])
 plot(a[, 2])
 
 
-# c.)
+# c.) ----
 num_obs <- dim(ar_2_matrix)[1]
 num_obs
 emp_errors <- (ar_2_matrix[3:num_obs, ]
@@ -140,4 +141,5 @@ emp_sigma_square <- apply(emp_errors, 2, mean)
 length(emp_sigma_square)
 plot(emp_sigma_square)
 
-# These measures are much to small... maybe this is where the accuracy was lost ...
+# These measures are much too small... 
+# ...maybe this is where the accuracy for a2 was lost ...
